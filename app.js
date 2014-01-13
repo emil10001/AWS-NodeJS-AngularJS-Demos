a9b345f2bef78f34c04f0a89446eef8d7c6a2c28
E John Feig <ejohn@feigdev.com>
2014-01-12 21:32:06 -0800

    check environment via environment variable

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ var io = require('socket.io').listen(3001)
     , aws_func = require('./aws_wrapper.js')
     , c = require('./constants.js');
 
-var aws = aws_func;
+var aws;
+
+if (!!process.env['AWS_ENV'] && !!process.env['AWS_ENV'] === 'dev')
+    aws = new aws_func();
+else
+    aws = aws_func;
 
 io.sockets.on('connection', function (socket) {
     // on connect
